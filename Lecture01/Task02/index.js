a05ec583bd4ff00b2a92c93a9c53e460e59ea310
Andrey Koshkarov <andrey.koshkarov@gmail.com>
2017-10-09 22:26:30 +0300

    Added comments for Task02 code and add unsubscribe functionality

--- Lecture01/Task02/index.js
+++ Lecture01/Task02/index.js
@@ -1,4 +1,5 @@
 function Lecture01_Task02() {
+    // Observer
     class Game {
         constructor(props) {
             this.name = props.name;
@@ -11,16 +12,36 @@ function Lecture01_Task02() {
             this.result = undefined;
         }
 
+        // Subscribe
         bet(player, amount) {
             if (this.isStarted || this.isFnished) {
                 return;
             }
-            this.playersInfo.push({
+            const playerInfo = {
                 player: player,
                 amount: amount
-            });
+            };
+            this.playersInfo.push(playerInfo);
+            // Unsubscribe
+            const unbet = () => {
+                if (this.isStarted || this.isFnished) {
+                    return false;
+                }
+                const playerIndex = this.playersInfo.indexOf(playerInfo);
+                if (playerIndex !== -1) {
+                    this.playersInfo.splice(playerIndex, 1);
+                    return true;
+                }
+                return false
+            };
+
+            //return playersInfo.id;
+            return {
+                unbet: unbet
+            };
         }
 
+        // Publish
         finish(result) {
             if (this.isFnished || !this.isStarted) {
                 return;
@@ -38,6 +59,7 @@ function Lecture01_Task02() {
             this.isFnished = true;
         }
 
+        // Start game simulation
         start() {
             if (this.isStarted || this.isFnished) {
                 return;
@@ -46,17 +68,20 @@ function Lecture01_Task02() {
         }
     }
 
+    // Subscriber
     class Player {
         constructor(name) {
             this.name = name;
         }
 
+        // Method called by Observable on Publish
+        // Notifies player about result of game
         notify(gameName, gain) {
             console.log(`${this.name}'s gain in game ${gameName} is ${gain}$`);
         }
     }
 
-    // Games
+    // Games (Observables, Subjects)
     const game1 = new Game({
         name: 'Game_1',
         koef: 1.2
@@ -78,7 +103,7 @@ function Lecture01_Task02() {
         koef: 1.15
     });
 
-    // Players
+    // Players (Subsribers)
     const player1 = new Player('Petya');
     const player2 = new Player('Vasya');
     const player3 = new Player('Egor');
@@ -86,12 +111,14 @@ function Lecture01_Task02() {
     const player5 = new Player('Max');
     const player6 = new Player('Tony');
 
-    // Players make bets
+    // Players make bets (make subscription)
     game1.bet(player1, 100);
     game1.bet(player2, 50);
 
     game3.bet(player1, 70);
-    game3.bet(player3, 150);
+    const gameSubscription = game3.bet(player3, 150);
+    // Example of unsubscribing
+    gameSubscription.unbet();
 
     game5.bet(player1, 200);
     game5.bet(player4, 300);
