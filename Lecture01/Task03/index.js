164a3cf71eeda8af230f774dd100f3bb96485002
Andrey Koshkarov <andrey.koshkarov@gmail.com>
2017-10-11 02:00:04 +0300

    Task03 different pattern solution. Refactoring

--- Lecture01/Task03/index.js
+++ Lecture01/Task03/index.js
@@ -1,79 +1,161 @@
-function Lecture01_Task03() {
-    const rentSkiing = ({
-        height,
-        age,
-        gender
-    }, money, days) => {
-        /* Some functionality */
-        console.log('Rent skiing.');
-        return {
-            balance: money - 20 * days,
-            cheque: 'Rent_skiing_cheque',
-            skiing: {
-                name: 'skiing'
-            }
-        };
-    };
-    const buyTicketToSkyLift = (money, quantity) => {
-        /* Some functionality */
-        console.log('Buy ticket to sky lift.');
-        return {
-            balance: money - 10 * quantity,
-            cheque: 'Sky_lift_cheque',
-            tickets: {
-                quantity: quantity
-            }
-        };
-    };
-    const rentApprtment = ({
-        height,
-        age,
-        gender
-    }, money, days) => {
-        console.log('Rent an appartment.');
-        return {
-            balance: money - 100 * days,
-            cheque: 'Appartment_rent_check',
-            keys: 1
-        };
-    };
+// Declare used classes for chain of responsibility pattern
+class Chain {
+    constructor() {
+        this.nextInChain = undefined;
+    }
+    setNextInChain(nextInChain) {
+        this.nextInChain = nextInChain;
+    }
+    process() {}
+}
 
-    // Fasade
-    const skiResortTerminal = (personParams, money) => {
-        if (money < 130) {
-            return {
-                result: false,
-                message: 'you do not have enouph money'
+class RentSkiingClass extends Chain {
+    process(params, avaliableMoney, processingResult) {
+        const cost = 20;
+
+        const {
+            personInfo: {
+                age,
+                gender
             }
+        } = params;
+
+        let skiing = '';
+        if (avaliableMoney.value >= cost) {
+            skiing =
+                (age < 15 ? 'Child' : 'Mature') + '_' +
+                (gender === 'male' ? 'male' : 'female') + '_skiing';
+
+            avaliableMoney.value = avaliableMoney.value - cost;
+            processingResult.cheque.push('Rent_skiing_cheque');
+            processingResult.items.push(skiing);
+        } else {
+            processingResult.messages.push('Not enouph money rent skiing');
         }
-        let rentSkiingResult = rentSkiing(personParams, money, 1);
-        let buyTicketToSkyLiftResult =
-            buyTicketToSkyLift(rentSkiingResult.balance, 1);
-        let rentApprtmentResult = rentApprtment(personParams,
-            buyTicketToSkyLiftResult.balance, 1);
-        return {
-            result: true,
-            balance: rentApprtmentResult.balance,
-            check: `
-            ${rentSkiingResult.cheque}
-            ${buyTicketToSkyLiftResult.cheque}
-            ${rentApprtmentResult.cheque}
-        `,
-            items: [
-                buyTicketToSkyLiftResult.tickets
-            ]
-        };
-    };
 
-    // Person info
-    const personInfo = {
+        if (this.nextInChain) {
+            return this.nextInChain.process(params, avaliableMoney, processingResult);
+        } else {
+            return Object.assign({}, processingResult, {
+                cashback: avaliableMoney.value
+            });
+        }
+    }
+}
+
+class BuyTicketToSkyLiftClass extends Chain {
+    process(params, avaliableMoney, processingResult) {
+        const cost = 10;
+
+        const ticket = 'Ticket_to_sky_lift'
+        if (avaliableMoney.value >= cost) {
+            avaliableMoney.value = avaliableMoney.value - cost;
+            processingResult.cheque.push('Ticket_to_sky_lift_cheque');
+            processingResult.items.push(ticket);
+        } else {
+            processingResult.messages.push('Not enouph money to buy ticket to sky lift');
+        }
+
+        if (this.nextInChain) {
+            return this.nextInChain.process(params, avaliableMoney, processingResult);
+        } else {
+            return Object.assign({}, processingResult, {
+                cashback: avaliableMoney.value
+            });
+        }
+    }
+}
+
+class RentAppartmentClass extends Chain {
+    process(params, avaliableMoney, processingResult) {
+        const cost = 100;
+
+        const {
+            personInfo: {
+                height,
+                age,
+                gender
+            }
+        } = params;
+
+        const key = 'Appartment_key';
+        if (avaliableMoney.value >= cost) {
+            // Some logic to find appartment based on person height, age and gender
+
+            avaliableMoney.value = avaliableMoney.value - cost;
+            processingResult.cheque.push('Appartment_rent_cheque');
+            processingResult.items.push(key);
+        } else {
+            processingResult.messages.push('Not enouph money to rent appartment');
+        }
+
+        if (this.nextInChain) {
+            return this.nextInChain.process(params, avaliableMoney, processingResult);
+        } else {
+            return Object.assign({}, processingResult, {
+                cashback: avaliableMoney.value
+            });
+        }
+    }
+}
+
+// Init chaining logic
+const rentSkiing = new RentSkiingClass();
+const buyTicketToSkyLift = new BuyTicketToSkyLiftClass();
+const rentApprtment = new RentAppartmentClass();
+
+rentSkiing.setNextInChain(buyTicketToSkyLift);
+buyTicketToSkyLift.setNextInChain(rentApprtment);
+
+// Fasade
+const skyResportTerminal = (params) =>
+    rentSkiing.process(params, {
+        value: params.money
+    }, {
+        cheque: [],
+        items: [],
+        messages: []
+    });
+
+// Different input params to check logic
+
+const params1 = {
+    personInfo: {
         height: 160,
         age: 30,
         gender: 'female'
-    };
+    },
+    money: 140
+};
 
-    // Use terminal and check result
-    console.log('=== Lecture01 - Task 03');
-    console.log(skiResortTerminal(personInfo, 125), '', '');
-    console.log(skiResortTerminal(personInfo, 140), '');
-}
+const params2 = {
+    personInfo: {
+        height: 100,
+        age: 10,
+        gender: 'female'
+    },
+    money: 25
+};
+
+const params3 = {
+    personInfo: {
+        height: 180,
+        age: 45,
+        gender: 'male'
+    },
+    money: 100
+};
+
+// Check logic
+const paramArray = [params1, params2, params3];
+const testFunc = (params) => {
+    console.log('******************');
+
+    console.log('PARAMS: ', params);
+    const processingResult = skyResportTerminal(params);
+    console.log('PROCESSING RESULT', processingResult);
+
+};
+
+console.log('=== Lecture01 - Task 03');
+paramArray.forEach(testFunc);
